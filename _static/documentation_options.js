08ddce05f6fe307de3d417ce7715b175e5aff7a5
Twiddly <pew@pewpew.moe>
2018-05-26 15:16:28 +0200

    update docs for 0.0.14;

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '0.0.14#126',
+    VERSION: '0.0.14#127',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
